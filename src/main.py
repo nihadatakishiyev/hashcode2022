92bbf6e13895d8196e0bde8a5d5bb32b14e98070
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2022-02-24 19:54:56 +0400

    init

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     files = utils.read_input_file_names()
     print(files)
     for i in files:
-        data = open('in/'+i)
+        data = open('src/in/'+i)
         utils.write_to_file(i, data.read())
--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,9 @@ from os import listdir
 
 
 def read_input_file_names():
-    return [f for f in listdir('in')]
+    return [f for f in listdir('src/in')]
 
 
 def write_to_file(file, content):
-    writable = open('out/' + file, 'w')
+    writable = open('src/out/' + file, 'w')
     writable.write(content)
