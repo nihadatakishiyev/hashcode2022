cec550e790fd9994f8efb4e08bb49a1c45cf4f48
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2022-02-25 00:15:29 +0400

    models, and parse input to format

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import sys
+
 import utils
 import models
 
@@ -8,6 +10,7 @@ if __name__ == '__main__':
         C, P = data.readline().split()
         contributors = []
         projects = []
+        last_day = 0
         for j in range(0, int(C)):
             name, num_skills = data.readline().split()
             skills = {}
@@ -15,7 +18,7 @@ if __name__ == '__main__':
                 s_name, level = data.readline().split()
                 skills[s_name] = int(level)
 
-            contributors.append(models.Contributor(name, num_skills, skills))
+            contributors.append(models.Contributor(name, int(num_skills), skills))
 
         for k in range(0, int(P)):
             p_name, deadline, score, b_before, num_roles = data.readline().split()
@@ -24,5 +27,60 @@ if __name__ == '__main__':
                 s_name, level = data.readline().split()
                 roles[s_name] = int(level)
 
-            projects.append(models.Project(p_name, deadline, score, b_before, num_roles, roles))
+            last_day += int(deadline)
+            projects.append(models.Project(p_name, int(deadline), int(score), int(b_before), int(num_roles), roles))
 
+        current_day = 0
+        print(contributors)
+        print(projects)
+        print(last_day)
+
+        while True:
+            current_day += 1
+            for p in projects:
+                if not p.is_started and not p.is_completed:
+                    hasAll = 0
+                    for role in p.roles:
+                        for conts in contributors:
+                            if not conts.isBusy:
+                                if conts.skill_level(role) >= p.roles[role]:
+                                    conts.start_working()
+                                    p.add_contributor(conts)
+                                    hasAll += 1
+                                elif p.has_higher_skill(role, p.roles[role]) and conts.skill_level(role)+1 >= p.roles[role]:
+                                    conts.start_working()
+                                    p.add_contributor(conts)
+                                    hasAll += 1
+
+                    if hasAll == p.num_roles:
+                        p.start()
+                    else:
+                        for cont in p.contributors:
+                            cont.stop_working()
+                        p.clear_contributors()
+
+            for p in projects:
+                if p.is_started and not p.is_completed:
+                    p.add_day()
+                    if p.is_completed:
+                        for index, skill in enumerate(p.roles):
+                            p.contributors[index].increase_skill(skill)
+                            p.contributors[index].stop_working()
+
+            completed = [p for p in projects if p.is_completed == True]
+
+            if current_day == last_day:
+                writable = open('src/out/' + i, 'w')
+                oldstdout = sys.stdout
+                sys.stdout = writable  # Change the standard output to the file we created.
+                print(len(completed))
+                for p in completed:
+                    print(p.name)
+                    names = ''
+                    for con in p.contributors:
+                        names += con.name + ' '
+
+                    print(names)
+
+                sys.stdout = oldstdout  # Reset the standard output to its original value
+                break
--- src/models.py
+++ src/models.py
@@ -6,7 +6,10 @@ class Contributor:
         self.isBusy = False
 
     def increase_skill(self, skill):
-        self.skills[skill] += 1
+        if skill in self.skills:
+            self.skills[skill] += 1
+        else:
+            self.skills[skill] = 1
 
     def start_working(self):
         self.isBusy = True
@@ -15,10 +18,14 @@ class Contributor:
         self.isBusy = False
 
     def skill_level(self, role):
-        return self.skills[role]
+        if role in self.skills:
+            return self.skills[role]
+        else:
+            return 0
 
     def __repr__(self):
         return str(self.name)
+
 
 class Skill:
     def __init__(self, name, level):
@@ -37,6 +44,37 @@ class Project:
         self.b_before = b_before
         self.num_roles = num_roles
         self.roles = roles
+        self.is_completed = False
+        self.is_feasible = False
+        self.day = 0
+        self.is_started = False
+        self.contributors = []
+
+    def start(self):
+        self.is_started = True
+
+    def add_day(self):
+        self.day += 1
+        self.is_completed = self.completed()
+
+    def add_contributor(self, contributor):
+        self.contributors.append(contributor)
+
+    def clear_contributors(self):
+        self.contributors = []
+
+    def has_higher_skill(self, skill, level):
+        has = False
+        for i in self.contributors:
+            if i.skill_level(skill) >= level:
+                has = True
+                break
+
+        return has
+
+    def completed(self):
+        # print(self.name+ " : " + str(self.day) + " : " + str(self.deadline) + "   == " + str(self.day == self.deadline))
+        return self.day == self.deadline
 
     def __repr__(self):
-        return str(self.name) + ' : ' + str(self.roles)
+        return str(self.name) + ' ' + str(self.score) + ' : ' + str(self.roles)
